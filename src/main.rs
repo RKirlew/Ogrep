e5ebd7a3c1cee73eba44055658757a88f807e3b8
Raheem <kirlewraheem@gmail.com>
2026-05-07 22:38:52 -0500

    added colors

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,19 @@ mod file;
 mod ogrep;
 use clap::Parser;
 use cli::Args;
-
+use colored::Colorize;
 fn main() {
     let args = Args::parse();
 
     match file::read_to_string(&args.file) {
         Ok(contents) => {
             let results = ogrep::search(&contents, &args.query, args.ignore_case, args.whole_word);
+            let scoff = "Count:";
             if args.count {
-                println!("{}", results.len());
+                println!("{} {}", scoff.red(), results.len());
             } else {
                 for line in results {
-                    println!("{}", line);
+                    println!("{}", line.green().bold());
                 }
             }
         }
